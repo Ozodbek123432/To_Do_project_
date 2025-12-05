5c6199ab21eaa9ca1065c4eb0009d64f4a038c8d
cyuber.devoloper <sabdumajid91@gmail.com>
2025-12-05 18:04:13 +0500

    yangii versay

--- BooC_Home/urls.py
+++ BooC_Home/urls.py
@@ -1,12 +1,12 @@
 # book/urls.py  (yoki login_app/urls.py)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from book.views import *
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,    # ← LOGIN
-    TokenRefreshView,       # ← yangi access token olish
+    TokenObtainPairView,
+    TokenRefreshView, TokenVerifyView,
 )
 
 urlpatterns = [
@@ -17,23 +17,6 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='login qilish'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='logo qilish'),
+    path('menu/', include("book.urls")),
 
-    # ===================== GENRE =====================
-
-    path('genres/', GenreListView.as_view(), name='korish'),
-    path('genres/create/', GenreCreateView.as_view(), name='korish'),
-
-    # ======================BOOC ==========================
-
-    path('book/', BookDataGetView.as_view(), name='yangilash'),
-    path('book/create/', BookDataCreateView.as_view(), name='korish'),
-    path('book/<int:pk>/', BookDataPutAndPatch.as_view(), name='ozgartish'),
-    path('book/<int:pk>/delete/',BookDataDeleteView.as_view(), name='ooxhrish'),
-
-    # ===================== AUTHOR =====================
-
-    path('authors/', AuthorDataGetView.as_view(), name='korish'),
-    path('authors/create/', AuthorDataCreateView.as_view(), name='yangilash'),
-    path('authors/<int:pk>/', AuthorDataPutAndPatch.as_view(), name='ozgartish'),
-    path('authors/<int:pk>/delete/', AuthorDataDeleteView.as_view(), name='ooxhrish'),  #
 ]
--- book/urls.py
+++ book/urls.py
@@ -13,8 +13,28 @@
 # # ModelViewSet uchun router
 # router = DefaultRouter()
 # router.register(r'books', BookModelViewSet, basename='book')
-#
-# urlpatterns = [
+from django.urls import path
+from book.views import *
+urlpatterns = [
+
+# ===================== GENRE =====================
+
+path('genres/', GenreListView.as_view(), name='korish'),
+path('genres/create/', GenreCreateView.as_view(), name='korish'),
+
+# ======================BOOC ==========================
+
+path('book/', BookDataGetView.as_view(), name='yangilash'),
+path('book/create/', BookDataCreateView.as_view(), name='korish'),
+path('book/<int:pk>/', BookDataPutAndPatch.as_view(), name='ozgartish'),
+path('book/<int:pk>/delete/', BookDataDeleteView.as_view(), name='ooxhrish'),
+
+# ===================== AUTHOR =====================
+
+path('authors/', AuthorDataGetView.as_view(), name='korish'),
+path('authors/create/', AuthorDataCreateView.as_view(), name='yangilash'),
+path('authors/<int:pk>/', AuthorDataPutAndPatch.as_view(), name='ozgartish'),
+path('authors/<int:pk>/delete/', AuthorDataDeleteView.as_view(), name='ooxhrish'),  #
 #     # Author — APIView
 #     path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
 #     path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
@@ -26,4 +46,4 @@
 #
 #     # Book — ModelViewSet (router orqali)
 #     path('', include(router.urls)),  # → /books/, /books/1/, ...
-# ]
+]
